16b8093a7997ea415ad8f0ab4b862f46d0df597f
Nico Viljanen <nico.nikusti@email.com>
2022-10-28 22:51:19 +0300

    less clutter & routing to project details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { HomeComponent } from './modules/components/home/home.component';
 import { PortfolioComponent } from './modules/components/portfolio/portfolio.component';
 import { AboutMeComponent } from './modules/components/about-me/about-me.component';
 import { ContactMeComponent } from './modules/components/contact-me/contact-me.component';
+import { ProjectDetailsComponent } from './modules/components/project-details/project-details.component';
 
 const routes: Routes = [
   {path:'Home',component:HomeComponent},
   {path:'Portfolio',component:PortfolioComponent},
-  {path:'Project',component:PortfolioComponent},
+  {path:'Project',component:ProjectDetailsComponent},
   {path:'About',component:AboutMeComponent},
   {path:'Contact',component:ContactMeComponent},
   {path:'**',redirectTo:'/Home',pathMatch: 'full'}
